e189a6502ff48c54ea5f413ee08a70d2f22f5025
Wahyu Agus Arifin <itpohgero@gmail.com>
2024-11-05 14:23:57 +0700

    permission

--- lib/app/binding/onboarding_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:ftwv_saqu/view/onboarding/controller/onboarding_controller.dart';
-import 'package:get/get.dart';
-
-class OnboardingBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<OnboardingController>(() => OnboardingController());
-  }
-}
--- lib/app/route/app_routes.dart
+++ lib/app/route/app_routes.dart
@@ -2,21 +2,14 @@ import 'package:ftwv_saqu/app/route/screen.dart';
 import 'package:get/get.dart';
 import '../../view/home/pages/home_page.dart';
 import '../../view/login/pages/login_page.dart';
-import '../../view/onboarding/pages/onboarding_page.dart';
 import '../../view/splash/pages/splash_page.dart';
 import '../binding/home_binding.dart';
 import '../binding/login_binding.dart';
-import '../binding/onboarding_binding.dart';
 import '../binding/splash_binding.dart';
 
 class AppRoutes {
 
   static List<GetPage> getPages() => [
-    GetPage(
-      name: Screen.onboarding,
-      page: () => const OnboardingView(),
-      binding: OnboardingBinding(),
-    ),
     GetPage(
       name: Screen.login,
       page: () => const LoginPage(),
